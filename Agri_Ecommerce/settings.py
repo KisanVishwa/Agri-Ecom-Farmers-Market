278d9f8309df51a403c08ce86782604cef4077c6
Agri-Ecom <Agricultureecom@gmail.com>
2024-12-08 06:51:32 +0530

    Changes env

--- Agri_Ecommerce/settings.py
+++ Agri_Ecommerce/settings.py
@@ -161,12 +161,3 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 SESSION_COOKIE_AGE = 86400  # 24 hours in seconds
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # This is the key setting
 SESSION_SAVE_EVERY_REQUEST = True
-
-# Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'agricultureecom@gmail.com'
-EMAIL_HOST_PASSWORD = 'znpavrizqlluavmi'  # Generate this from Google Account settings
-DEFAULT_FROM_EMAIL = 'agricultureecom@gmail.com'
